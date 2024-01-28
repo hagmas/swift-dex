a6b5b0589db1e8b17db1a4f80a552b2d7207a763
hagmas <hgmsk1985@gmail.com>
2024-01-28 22:42:59 +0100

    Add ImageTest

---
+++ ImageTest/ImageTest/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  ImageTest
+//
+//  Created by Masaki Haga on 2024/01/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("Mountain")
+            .resizable()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageTest/ImageTest/ImageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTestApp.swift
+//  ImageTest
+//
+//  Created by Masaki Haga on 2024/01/28.
+//
+
+import SwiftUI
+
+@main
+struct ImageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
